627f867419e38c82c0426e749769d17c453ecea6
Manuel Exposito <manuelexposito4d@gmail.com>
2021-10-03 19:08:44 +0200

    Creadas clases ArtistRepository y ArtistController

---
+++ Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/controller/ArtistController.java
@@ -0,0 +1,7 @@
+package com.trianasalesianos.dam.Trianafy.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ArtistController {
+}
---
+++ Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/repository/ArtistRepository.java
@@ -0,0 +1,7 @@
+package com.trianasalesianos.dam.Trianafy.repository;
+
+import com.trianasalesianos.dam.Trianafy.model.Artist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArtistRepository extends JpaRepository<Artist, Long> {
+}
