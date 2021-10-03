96a6b31fc28d3b00cae2c553a7fadf71f365d92b
DanielFB95 <fernandez.bedan20@triana.salesianos.edu>
2021-10-03 18:44:32 +0200

    Inicio

---
+++ Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/TrianafyApplication.java
@@ -0,0 +1,13 @@
+package com.trianasalesianos.dam.Trianafy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrianafyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrianafyApplication.class, args);
+	}
+
+}
---
+++ Trianafy/src/test/java/com/trianasalesianos/dam/Trianafy/TrianafyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trianasalesianos.dam.Trianafy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrianafyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
