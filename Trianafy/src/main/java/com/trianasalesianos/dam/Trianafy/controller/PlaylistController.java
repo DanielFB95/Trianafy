afcab4c7fc1bfced22c82b7e9379e46ac06a60fe
Manuel Exposito <manuelexposito4d@gmail.com>
2021-10-04 19:16:32 +0200

    Funcion HU-15 Borrar una playlist creada

--- Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/controller/PlaylistController.java
+++ Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/controller/PlaylistController.java
@@ -1,6 +1,11 @@
 package com.trianasalesianos.dam.Trianafy.controller;
 
+import com.trianasalesianos.dam.Trianafy.repository.PlaylistRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/playlist")
 @RequiredArgsConstructor
 public class PlaylistController {
+
+    private final PlaylistRepository repository;
+
+    /**
+     * Borrar playlist por ID
+     * @param id
+     * @return codigo de estado 204 NO CONTENT
+     */
+    @DeleteMapping("/{id}")
+    ResponseEntity<?> delete(@PathVariable Long id){
+
+        repository.deleteById(id);
+        return ResponseEntity.noContent()
+                .build();
+
+    }
+
 }
