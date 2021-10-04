d15a815d73f7dc1e99e644e2482fa75ca3ee2a77
Pablo <pabloseguravelasco@gmail.com>
2021-10-04 19:12:45 +0200

    Creada la rama HU-12

--- Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/controller/PlaylistController.java
+++ Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/controller/PlaylistController.java
@@ -1,11 +1,27 @@
 package com.trianasalesianos.dam.Trianafy.controller;
 
+import com.trianasalesianos.dam.Trianafy.model.Playlist;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/playlist")
 @RequiredArgsConstructor
 public class PlaylistController {
+
+    private final PlaylistRepository repository;
+
+    @GetMapping("/")
+    public List<Playlist> findAll(){
+
+        return repository.findAll().getSongs().lenght();
+
+    }
+
+
+
 }
