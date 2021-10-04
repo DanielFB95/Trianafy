2f8f85a402208bcc5db7e9fb55bf782d6fcb9e5f
Manuel Exposito <manuelexposito4d@gmail.com>
2021-10-04 19:04:21 +0200

    Funcion HU-08 Buscar una cancion por Id creada

--- Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/controller/SongController.java
+++ Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/controller/SongController.java
@@ -1,4 +1,33 @@
 package com.trianasalesianos.dam.Trianafy.controller;
 
+
+import com.trianasalesianos.dam.Trianafy.model.Song;
+import com.trianasalesianos.dam.Trianafy.repository.SongRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController("/song")
+@RequiredArgsConstructor
 public class SongController {
+
+
+    private final SongRepository repository;
+
+
+    /**
+     * Buscar cancion por ID
+     */
+    @GetMapping("/{id}")
+    ResponseEntity<Song> findOne(@PathVariable Long id){
+
+        return ResponseEntity.of(repository.findById(id));
+
+
+    }
+
 }
