2f2cd064c318fa288c48d53cffa6492edb2753dc
Pablo <pabloseguravelasco@gmail.com>
2021-10-04 18:43:56 +0200

    Realizada la rama HU-03

--- Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/controller/ArtistController.java
+++ Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/controller/ArtistController.java
@@ -1,7 +1,25 @@
 package com.trianasalesianos.dam.Trianafy.controller;
 
+import com.trianasalesianos.dam.Trianafy.model.Artist;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ArtistController {
+
+    @GetMapping("/")
+    public List<Artist> findAll(){
+
+        return repository.findAll();
+
+    }
+    @GetMapping("/{id}")
+    public Artist findOne(@PathVariable("id") Long id){
+
+        return repository.findById(id).orElse(null);
+    }
+
 }
