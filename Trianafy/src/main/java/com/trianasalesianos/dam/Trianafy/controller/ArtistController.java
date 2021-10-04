ed9bc95536a91bf89c3d50291f85211154da1337
Manuel Exposito <manuelexposito4d@gmail.com>
2021-10-04 18:17:47 +0200

    Funcion HU-02 Listar todos los artistas creada

--- Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/controller/ArtistController.java
+++ Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/controller/ArtistController.java
@@ -1,7 +1,29 @@
 package com.trianasalesianos.dam.Trianafy.controller;
 
+import com.trianasalesianos.dam.Trianafy.model.Artist;
+import com.trianasalesianos.dam.Trianafy.repository.ArtistRepository;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/artist")
+@RequiredArgsConstructor
 public class ArtistController {
+
+    private final ArtistRepository repository;
+
+    @GetMapping("/")
+    ResponseEntity<List<Artist>> findAll(){
+
+        return ResponseEntity.ok()
+                            .body(repository.findAll());
+
+    }
+
 }
