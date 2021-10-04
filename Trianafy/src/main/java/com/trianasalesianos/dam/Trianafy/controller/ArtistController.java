b634c13460a207277e4e000fded4cc50692989ac
Manuel Exposito <manuelexposito4d@gmail.com>
2021-10-04 18:34:57 +0200

    Funcion HU-05 Eliminar artista creada

--- Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/controller/ArtistController.java
+++ Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/controller/ArtistController.java
@@ -1,7 +1,31 @@
 package com.trianasalesianos.dam.Trianafy.controller;
 
+import com.trianasalesianos.dam.Trianafy.repository.ArtistRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/artist")
+@RequiredArgsConstructor
 public class ArtistController {
+
+    private final ArtistRepository repository;
+
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<?> delete(@PathVariable Long id){
+
+        repository.deleteById(id);
+
+        return ResponseEntity.noContent()
+                             .build();
+
+
+    }
+
+
 }
