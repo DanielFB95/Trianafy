10fdfb2492f540de1f35247c65554d1c455fcdc8
Daniel Fernández Bernal <fernandez.bedan20@triana.salesianos.edu>
2021-10-04 14:18:33 +0200

    HU-01

--- Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/controller/ArtistController.java
+++ Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/controller/ArtistController.java
@@ -1,7 +1,30 @@
 package com.trianasalesianos.dam.Trianafy.controller;
 
+import com.trianasalesianos.dam.Trianafy.model.Artist;
+import com.trianasalesianos.dam.Trianafy.repository.ArtistRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+
 @RestController
+@RequestMapping("/artist")
+@RequiredArgsConstructor
 public class ArtistController {
+
+    private final ArtistRepository repository;
+
+    @PostMapping("/")
+    public ResponseEntity<Artist> create(@RequestBody Artist n){
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(repository.save(n));
+
+    }
 }
