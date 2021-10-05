9fc2c108e8470d96624bf0eb241a33084e1aa62c
Pablo Segura Velasco <segura.vepab21@triana.salesianos.edu>
2021-10-05 14:11:33 +0200

    Creada la rama HU-17

--- Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/controller/PlaylistController.java
+++ Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/controller/PlaylistController.java
@@ -1,6 +1,11 @@
 package com.trianasalesianos.dam.Trianafy.controller;
 
+import com.trianasalesianos.dam.Trianafy.model.Playlist;
+import com.trianasalesianos.dam.Trianafy.repository.PlaylistRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/playlist")
 @RequiredArgsConstructor
 public class PlaylistController {
-}
+
+    private final PlaylistRepository repository;
+
+    @GetMapping("/{id}/")
+    public ResponseEntity<Playlist> findOne(@PathVariable Long id) {
+
+
+        return  ResponseEntity
+                .of(repository.findById(id));
+
+
+                }
+
+    }
+
