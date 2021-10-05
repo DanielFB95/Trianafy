f7fb9c69b95745a1072e806e52a78421ba03a557
Pablo Segura Velasco <segura.vepab21@triana.salesianos.edu>
2021-10-05 13:20:19 +0200

    Creada la rama HU-10

--- Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/controller/SongController.java
+++ Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/controller/SongController.java
@@ -1,4 +1,14 @@
 package com.trianasalesianos.dam.Trianafy.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 public class SongController {
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
