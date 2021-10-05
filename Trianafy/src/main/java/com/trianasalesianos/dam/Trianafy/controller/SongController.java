ffba51b548ded4652cdc751019476316e7e32924
Pablo <pabloseguravelasco@gmail.com>
2021-10-05 18:06:12 +0200

    Realizada rama HU-06 POST SongController Dto y CreateSongDto

--- Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/controller/SongController.java
+++ Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/controller/SongController.java
@@ -1,4 +1,33 @@
 package com.trianasalesianos.dam.Trianafy.controller;
 
+import com.trianasalesianos.dam.Trianafy.dto.CreateSongDto;
+import com.trianasalesianos.dam.Trianafy.model.Song;
+import com.trianasalesianos.dam.Trianafy.repository.SongRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public class SongController {
+
+
+    private final SongRepository repository;
+    private final SongDtoConverter dtoConverter;
+
+    
+
+    @PostMapping("/")
+
+    public ResponseEntity<Song> create(@RequestBody CreateSongDto newSong) {
+
+
+
+        Song newSong = dtoConverter.createSongDtoToSong(newSong);
+
+
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(repository.save(newSong));
+    }
 }
--- Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/dto/CreateSongDto.java
+++ Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/dto/CreateSongDto.java
@@ -1,4 +1,22 @@
 package com.trianasalesianos.dam.Trianafy.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateSongDto {
+
+    private Long artistId;
+
+    private String title;
+    private String album;
+    private int year;
+
+
+
+
 }
