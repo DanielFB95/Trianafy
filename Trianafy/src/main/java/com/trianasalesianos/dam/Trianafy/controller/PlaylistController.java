8c5609ebfb85ef5cfd0d77d1f2c68d56734ac518
Pablo Segura Velasco <segura.vepab21@triana.salesianos.edu>
2021-10-05 13:09:22 +0200

    Añadido ResponseEntity en PlaylistController

--- Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/controller/PlaylistController.java
+++ Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/controller/PlaylistController.java
@@ -1,7 +1,9 @@
 package com.trianasalesianos.dam.Trianafy.controller;
 
 import com.trianasalesianos.dam.Trianafy.model.Playlist;
+import com.trianasalesianos.dam.Trianafy.repository.PlaylistRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +18,12 @@ public class PlaylistController {
     private final PlaylistRepository repository;
 
     @GetMapping("/")
-    public List<Playlist> findAll(){
+    public ResponseEntity <List<Playlist>> findAll(){
 
-        return repository.findAll()
+        
+        return ResponseEntity.of( repository.findAll()
                 .getName()
-                .getId.getSongs().lenght();
+                .getId.getSongs().lenght());
 
     }
 
