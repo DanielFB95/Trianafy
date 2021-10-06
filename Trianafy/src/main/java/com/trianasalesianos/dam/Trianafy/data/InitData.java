8d1eff84f86e42201a7db139d9671a468687bb2b
Daniel Fernández Bernal <fernandez.bedan20@triana.salesianos.edu>
2021-10-06 13:16:14 +0200

    rama para borrar

--- Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/data/InitData.java
+++ Trianafy/src/main/java/com/trianasalesianos/dam/Trianafy/data/InitData.java
@@ -1,4 +1,61 @@
 package com.trianasalesianos.dam.Trianafy.data;
 
+import com.trianasalesianos.dam.Trianafy.model.Artist;
+import com.trianasalesianos.dam.Trianafy.model.Playlist;
+import com.trianasalesianos.dam.Trianafy.model.Song;
+import com.trianasalesianos.dam.Trianafy.repository.ArtistRepository;
+import com.trianasalesianos.dam.Trianafy.repository.PlaylistRepository;
+import com.trianasalesianos.dam.Trianafy.repository.SongRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
 public class InitData {
+
+    private final SongRepository songRepo;
+    private final ArtistRepository artistRepo;
+    private final PlaylistRepository playlistRepo;
+
+
+    @PostConstruct
+    public void init() {
+
+        artistRepo.saveAll(
+
+                Arrays.asList(
+                        new Artist("Lil Nas X"),
+                        new Artist("Twenty One Pilots"),
+                        new Artist("Billie Eilish")
+                )
+        );
+
+        songRepo.saveAll(
+
+                Arrays.asList(
+                        new Song("Jumpsuit", artistRepo.getById(2L), "Trench", "2018"),
+                        new Song("Call me by your Name", artistRepo.getById(1L), "Montero", "2021"),
+                        new Song("Bad Guy", artistRepo.getById(3L), "When we fall asleep, where do we go?", "2019")
+                )
+
+        );
+
+        List<Song> ejemplos = new ArrayList<Song>();
+        ejemplos.add(songRepo.getById(2L));
+        ejemplos.add(songRepo.getById(3L));
+
+        playlistRepo.save(
+                new Playlist("Una playlist", "Esta es una playlist de prueba", ejemplos)
+        );
+
+
+    }
+
+
 }
+
